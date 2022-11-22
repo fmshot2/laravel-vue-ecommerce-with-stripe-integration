b4e45ee92e4cab8c97b46c59190aec482f8519c9
fmshot2 <femishotolaa@gmail.com>
2022-11-22 22:17:34 +0100

    delete category done

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -85,25 +85,39 @@ class CategoryController extends BaseController
     }
 
     /**
- * @param Request $request
- * @return \Illuminate\Http\RedirectResponse
- * @throws \Illuminate\Validation\ValidationException
- */
-public function update(Request $request)
-{
-    $this->validate($request, [
-        'name'      =>  'required|max:191',
-        'parent_id' =>  'required|not_in:0',
-        'image'     =>  'mimes:jpg,jpeg,png|max:1000'
-    ]);
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'name'      =>  'required|max:191',
+            'parent_id' =>  'required|not_in:0',
+            'image'     =>  'mimes:jpg,jpeg,png|max:1000'
+        ]);
 
-    $params = $request->except('_token');
+        $params = $request->except('_token');
 
-    $category = $this->categoryRepository->updateCategory($params);
+        $category = $this->categoryRepository->updateCategory($params);
 
-    if (!$category) {
-        return $this->responseRedirectBack('Error occurred while updating category.', 'error', true, true);
+        if (!$category) {
+            return $this->responseRedirectBack('Error occurred while updating category.', 'error', true, true);
+        }
+        return $this->responseRedirectBack('Category updated successfully', 'success', false, false);
     }
-    return $this->responseRedirectBack('Category updated successfully' ,'success',false, false);
-}
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function delete($id)
+    {
+        $category = $this->categoryRepository->deleteCategory($id);
+
+        if (!$category) {
+            return $this->responseRedirectBack('Error occurred while deleting category.', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.categories.index', 'Category deleted successfully', 'success', false, false);
+    }
 }
