4f7b1b98f1912b9ad183f8f65f06ea40324f28f6
fmshot2 <femishotolaa@gmail.com>
2022-11-28 19:56:34 +0100

    finshed brand module

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,8 +3,12 @@
 namespace App\Providers;
 use App\Contracts\CategoryContract;
 use App\Repositories\CategoryRepository;
+
 use App\Contracts\AttributeContract;
 use App\Repositories\AttributeRepository;
+
+use App\Contracts\BrandContract;
+use App\Repositories\BrandRepository;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -13,7 +17,7 @@ class RepositoryServiceProvider extends ServiceProvider
     protected $repositories = [
         CategoryContract::class         =>          CategoryRepository::class,
         AttributeContract::class        =>          AttributeRepository::class,
-
+        BrandContract::class            =>          BrandRepository::class,
     ];
 
     /**
